664ab6edaab61a10e020e42482c2967ac7f2dcc0
Leonardo Garcia <lgarcia-@student.42lyon.fr>
2020-05-26 17:28:53 +0200

    kata02 solution with datetime

--- day00/ex05/kata02.py
+++ day00/ex05/kata02.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import datetime
 
 
 def main():
     time = (3, 30, 2019, 9, 25)
-    print('{0[3]:02}/{0[4]:02}/{0[2]} {0[0]:02}:{0[1]:02}'.format(time))
+    d = datetime.datetime(time[2], time[3], time[4], time[0], time[1])
+    print('{:%m/%d/%Y %H:%M}'.format(d))
+    # print('{0[3]:02}/{0[4]:02}/{0[2]} {0[0]:02}:{0[1]:02}'.format(time))
 
 
 if __name__ == "__main__":
